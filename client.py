220b8624a98b417b20be02de42bbf70de57c9957
xjw__ <72041194+Ayloten@users.noreply.github.com>
2021-03-16 12:07:35 -0400

    OOGABOOGA ROT13
    nice

--- client.py
+++ client.py
@@ -2,23 +2,30 @@ import socket
 import base64
 import sys
 import contextlib
+import time
+import codecs
+
 
 sys.tracebacklimit = 1
 
 # take the server name and port name.
-host = 'YOUR IP HERE'
+host = ''
 port = 5000
 
 # create a socket at client side 
 # using TCP / IP protocol 
 s = socket.socket(socket.AF_INET, 
-				socket.SOCK_STREAM) 
+				socket.SOCK_STREAM)
+
+time.sleep(3)
 
 # connect it to server and port 
 # number on local computer.
 with contextlib.suppress(ConnectionRefusedError):
-	s.connect(('YOUR IP HERE', port)) 
+	s.connect(('', port)) 
 
+	
+	
 # receive message string from 
 # server, at a time 1024 B
 with contextlib.suppress(OSError):
@@ -31,12 +38,18 @@ with contextlib.suppress(NameError):
 		recievedmsg = msg.decode()
 		msg = s.recv(1024)
 
-# decoding base64
-with contextlib.suppress(NameError):
-	p = recievedmsg
-	d = base64.b64decode(p)
-	f = d.decode("utf-8")
-	print(f)
+ # decoding base64
+try:
+	with contextlib.suppress(NameError):
+		p = recievedmsg
+		d = base64.b64decode(p)
+		f = d.decode("utf-8")
+		print(f)
+except:
+	with contextlib.suppress(NameError):
+		p = recievedmsg
+		d = codecs.decode(f"{p}", 'rot_13')
+		print(d)
 
-# disconnect the client a 
+# disconnect the client a
 s.close()
--- server.py
+++ server.py
@@ -1,16 +1,23 @@
 import socket 
 import base64
+import codecs
+
+# rot 13 time
+rot13bool = input("would you like to use ROT13? Y for Yes, N for no.")
 
 # asking for the message using input()
 message = input("Message? ")
 
 # im gonna encode it into base 64 now so i dont have to worry about it later
-message_bytes = message.encode('ascii')
-base64_bytes = base64.b64encode(message_bytes)
-base64_message = base64_bytes.decode('ascii')
+if rot13bool == 'N':
+	message_bytes = message.encode('ascii')
+	base64_bytes = base64.b64encode(message_bytes)
+	base64_message = base64_bytes.decode('ascii')
+else:
+	rot13_bytes = codecs.encode(f'{message}', 'rot_13')
 
 # take the server name and port name 
-host = 'YOUR IP HERE'
+host = ''
 port = 5000
 
 # create a socket at server side 
@@ -31,8 +38,11 @@ s.listen(1)
 c, addr = s.accept() 
 
 # send message to the client after 
-# encoding into binary string 
-c.send(bytes(f"{base64_message}", encoding="ascii"))
+# encoding into binary string
+if rot13bool == "N": 
+	c.send(bytes(f"{base64_message}", encoding="ascii"))
+else:
+	c.send(bytes(f"{rot13_bytes}", encoding="ascii"))
 
 c.close()
 
@@ -40,14 +50,20 @@ keep = input("Want to send another message? Y/N: ")
 if keep == "N":
 	c.close()
 if keep == "Y":
+	rot13bool = input("would you like to use ROT13? Y for Yes, N for no.")
+
+# asking for the message using input()
 	message = input("Message? ")
 
-	# im gonna encode it into base 64 now so i dont have to worry about it later
-	message_bytes = message.encode('ascii')
-	base64_bytes = base64.b64encode(message_bytes)
-	base64_message = base64_bytes.decode('ascii')
+# im gonna encode it into base 64 now so i dont have to worry about it later
+	if rot13bool == 'N':
+		message_bytes = message.encode('ascii')
+		base64_bytes = base64.b64encode(message_bytes)
+		base64_message = base64_bytes.decode('ascii')
+	else:
+		rot13_bytes = codecs.encode(f'{message}', 'rot_13')
 
-	host = 'local host'
+	host = ''
 	port = 5000
 
 	# create a socket at server side 
@@ -67,25 +83,35 @@ if keep == "Y":
 	# connection 
 	c, addr = s.accept() 
 
-	# display client address 
-	print("Found Reciever At:", str(addr)) 
+	# display client address
+	if addr != host: 
+		print("Found Reciever At:", str(addr))
+	else:
+		print("self connected.") 
 
-	# send message to the client after 
-	# encoding into binary string 
-	c.send(bytes(f"{base64_message}", encoding="ascii"))
+	if rot13bool == "N": 
+		c.send(bytes(f"{base64_message}", encoding="ascii"))
+	else:
+		c.send(bytes(f"{rot13_bytes}", encoding="ascii"))
 
 	c.close()
 
 	
 while keep == "Y":
+	rot13bool = input("would you like to use ROT13? Y for Yes, N for no.")
+
+	# asking for the message using input()
 	message = input("Message? ")
 
 	# im gonna encode it into base 64 now so i dont have to worry about it later
-	message_bytes = message.encode('ascii')
-	base64_bytes = base64.b64encode(message_bytes)
-	base64_message = base64_bytes.decode('ascii')
+	if rot13bool == 'N':
+		message_bytes = message.encode('ascii')
+		base64_bytes = base64.b64encode(message_bytes)
+		base64_message = base64_bytes.decode('ascii')
+	else:
+		rot13_bytes = codecs.encode(f'{message}', 'rot_13')
 
-	host = 'local host'
+	host = ''
 	port = 5000
 
 	# create a socket at server side 
@@ -106,9 +132,15 @@ while keep == "Y":
 	c, addr = s.accept() 
 
 	# display client address 
-	print("CONNECTION FROM:", str(addr)) 
+	if addr != host:
+		print("CONNECTION FROM: ", str(addr)) 
+	else:
+		print("self connected")
 
-	c.send(bytes(f"{base64_message}", encoding="ascii"))
+	if rot13bool == "N": 
+		c.send(bytes(f"{base64_message}", encoding="ascii"))
+	else:
+		c.send(bytes(f"{rot13_bytes}", encoding="ascii"))
 
 	c.close()
 
