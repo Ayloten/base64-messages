a93b2a0341341186480522af2cb95b08dd3305f1
xjw__ <72041194+Ayloten@users.noreply.github.com>
2021-03-18 11:35:10 -0400

    MAJOR BUG FIX
    Fixed a MAJOR bug where the server would only send in ROT13 because the variable wasn't equal to what i thought it was equal to. anyways its fixed now.

--- server.py
+++ server.py
@@ -1,151 +1,72 @@
-import socket 
+import tkinter as tk
+from tkinter import ttk
+import socket
 import base64
 import codecs
 
-# rot 13 time
-rot13bool = input("would you like to use ROT13? Y for Yes, N for no.")
 
-# asking for the message using input()
-message = input("Message? ")
+window = tk.Tk()
+window.title("CarbonMessages ALPHA")
+window.minsize(400,200)
 
-# im gonna encode it into base 64 now so i dont have to worry about it later
-if rot13bool == 'N':
-	message_bytes = message.encode('ascii')
-	base64_bytes = base64.b64encode(message_bytes)
-	base64_message = base64_bytes.decode('ascii')
-else:
-	rot13_bytes = codecs.encode(f'{message}', 'rot_13')
+ROT13bool = tk.IntVar()
+chkbox = tk.Checkbutton(text="Use ROT13?", variable=ROT13bool)
+chkbox.pack()
 
-# take the server name and port name 
-host = ''
-port = 5000
-
-# create a socket at server side 
-# using TCP / IP protocol 
-s = socket.socket(socket.AF_INET, 
-				socket.SOCK_STREAM) 
-
-# bind the socket with server 
-# and port number 
-s.bind(('', port)) 
-
-# allow maximum 1 connection to 
-# the socket 
-s.listen(1) 
-
-# wait till a client accept 
-# connection 
-c, addr = s.accept() 
-
-# send message to the client after 
-# encoding into binary string
-if rot13bool == "N": 
-	c.send(bytes(f"{base64_message}", encoding="ascii"))
-else:
-	c.send(bytes(f"{rot13_bytes}", encoding="ascii"))
-
-c.close()
-
-keep = input("Want to send another message? Y/N: ")
-if keep == "N":
-	c.close()
-if keep == "Y":
-	rot13bool = input("would you like to use ROT13? Y for Yes, N for no.")
-
-# asking for the message using input()
-	message = input("Message? ")
-
-# im gonna encode it into base 64 now so i dont have to worry about it later
-	if rot13bool == 'N':
-		message_bytes = message.encode('ascii')
+def clickMe():
+	if (ROT13bool.get()) == 0:
+		message_bytes = name.get().encode('ascii')
 		base64_bytes = base64.b64encode(message_bytes)
 		base64_message = base64_bytes.decode('ascii')
 	else:
-		rot13_bytes = codecs.encode(f'{message}', 'rot_13')
+		rot13_bytes = codecs.encode(f'{name.get()}', 'rot_13')
 
 	host = ''
 	port = 5000
 
-	# create a socket at server side 
-	# using TCP / IP protocol 
-	s = socket.socket(socket.AF_INET, 
-				socket.SOCK_STREAM) 
+	# create a socket at server side
+	# using TCP / IP protocol
+	s = socket.socket(socket.AF_INET,
+					socket.SOCK_STREAM)
 
-	# bind the socket with server 
-	# and port number 
-	s.bind(('', port)) 
+	# bind the socket with server
+	# and port number
+	s.bind(('', port))
 
-	# allow maximum 1 connection to 
-	# the socket 
-	s.listen(1) 
+	# allow maximum 1 connection to
+	# the socket
+	s.listen(1)
 
-	# wait till a client accept 
-	# connection 
-	c, addr = s.accept() 
+	# wait till a client accept
+	# connection
+	c, addr = s.accept()
 
-	# display client address
-	if addr != host: 
-		print("Found Reciever At:", str(addr))
-	else:
-		print("self connected.") 
-
-	if rot13bool == "N": 
+	if (ROT13bool.get()) == 0:
 		c.send(bytes(f"{base64_message}", encoding="ascii"))
 	else:
 		c.send(bytes(f"{rot13_bytes}", encoding="ascii"))
 
 	c.close()
 
-	
-while keep == "Y":
-	rot13bool = input("would you like to use ROT13? Y for Yes, N for no.")
 
-	# asking for the message using input()
-	message = input("Message? ")
 
-	# im gonna encode it into base 64 now so i dont have to worry about it later
-	if rot13bool == 'N':
-		message_bytes = message.encode('ascii')
-		base64_bytes = base64.b64encode(message_bytes)
-		base64_message = base64_bytes.decode('ascii')
-	else:
-		rot13_bytes = codecs.encode(f'{message}', 'rot_13')
-
-	host = ''
-	port = 5000
-
-	# create a socket at server side 
-	# using TCP / IP protocol 
-	s = socket.socket(socket.AF_INET, 
-				socket.SOCK_STREAM) 
-
-	# bind the socket with server 
-	# and port number 
-	s.bind(('', port)) 
-
-	# allow maximum 1 connection to 
-	# the socket 
-	s.listen(1) 
-
-	# Wanit till a client accept 
-	# connection 
-	c, addr = s.accept() 
-
-	# display client address 
-	if addr != host:
-		print("CONNECTION FROM: ", str(addr)) 
-	else:
-		print("self connected")
-
-	if rot13bool == "N": 
-		c.send(bytes(f"{base64_message}", encoding="ascii"))
-	else:
-		c.send(bytes(f"{rot13_bytes}", encoding="ascii"))
-
-	c.close()
-
-	
+label = tk.Label(window, text = "Enter Your message")
+label.pack()
 
 
 
-# disconnect the server  
+
+
+name = tk.StringVar()
+messageEntered = ttk.Entry(window, width = 15, textvariable = name)
+messageEntered.pack()
+
+
+button = ttk.Button(window, text = "Send", command = clickMe)
+button.pack()
+
+
+MyTitle2 = tk.Label(window, text="CURRENTLY IN BETA", font="Hevetica 12 bold", fg='black', bg='white')
+MyTitle2.pack()
+
+window.mainloop()
